67c968f5f7fe67a81f5b51e68148e03186e04c52
bats11 <70653789+bats11@users.noreply.github.com>
2026-01-07 01:03:39 +0100

    update

--- js/scene-showcase.js
+++ js/scene-showcase.js
@@ -302,7 +302,7 @@ export function initShowcaseMap(resizeCallbacks) {
     ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
     scene.add(ambientLight);
 
-    directionalLight = new THREE.DirectionalLight(0xffffff, 2.8);
+    directionalLight = new THREE.DirectionalLight(0xffffff, 4);
     directionalLight.position.set(10, 20, 10);
     directionalLight.target.position.set(0, 0, 0);
     directionalLight.castShadow = true;
